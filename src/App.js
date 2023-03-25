a7ee15df9f654b48b3f53c2f8455699da1fbf4b6
Suyash Gaurav <102952185+SuyashGaurav@users.noreply.github.com>
2023-03-25 22:12:37 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import {Route} from 'react-router-dom';
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import "./App.css";
+
 export const App = () => {
   return (
     <>
       <Navbar />
-      <Home />
+    <Routes>
+    <Route exact path = "/" element = {<Home />}/>
+    <Route path = "/about" element = {<About />}/>
+    <Route path = "/contact" element = {<Contact />}/>
+    <Route path = "/login" element = {<Login />}/>
+    <Route path = "/signup" element = {<Signup />}/>
+    </Routes>
     </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,30 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+  <button class="btn btn-default btn-lg"><i class="zmdi zmdi-settings zmdi-hc-spin"></i></button>
+    <NavLink className="navbar-brand" to="/">ULX</NavLink>
+    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button> */}
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ml-auto">
+        <li classNameName="nav-item">
+          <NavLink classNameName="nav-link" to="/about">About</NavLink>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/about">About</a>
+        <li classNameName="nav-item">
+          <NavLink classNameName="nav-link" to="/contact">Contact</NavLink>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/contact">Contact</a>
+        <li classNameName="nav-item">
+          <NavLink classNameName="nav-link" to="/login">Login</NavLink>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/login">Login</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/signup">Registration</a>
+        <li classNameName="nav-item">
+          <NavLink classNameName="nav-link" to="/signup">Registration</NavLink>
         </li>
       </ul>
     </div>
